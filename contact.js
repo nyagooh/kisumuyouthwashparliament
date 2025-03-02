730851cc648f5314f345ae82eb6b48b2e8196fc7
Dudimath <annmaina614@gmail.com>
2025-03-02 22:10:16 +0300

    update the map function by adding eventlistener

--- contact.js
+++ contact.js
@@ -1,13 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var map = L.map('leaflet-map').setView([-0.1022, 34.7617], 13); // Kisumu coordinates
 
-    // Initialize the map and set its view
-    var map = L.map('map').setView([-0.1022, 34.7617], 13); // Kisumu coordinates
-
-    // Add a tile layer (OpenStreetMap)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap contributors'
     }).addTo(map);
 
-    // Add a marker for Kisumu
     L.marker([-0.1022, 34.7617]).addTo(map)
         .bindPopup("<b>Kisumu, Kenya</b><br>A city by Lake Victoria.")
         .openPopup();
+});
